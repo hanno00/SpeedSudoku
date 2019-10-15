49baa1ce69ab2c9805bad07fa2efa8b25aaa44f3
Yoram van Spijk <yoramvanspijk1994@hotmail.com>
2019-10-16 01:03:06 +0200

    tfw je niet genoeg oplet in p4 van jaar 1 en dit dus 200000x lastiger is dan het zou moeten zijn
    Ik ga later nog een keer door joe

---
+++ SudokuServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using SudokuLogic;
+
+
+namespace SudokuServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // setup server connection
+            List<TcpClient> clients;
+
+
+            // create lobby
+            TcpListener server = new TcpListener(IPAddress.Loopback, 5555);
+            clients = new List<TcpClient>();
+            server.Start();
+            Console.WriteLine($">> Server Online at {server.LocalEndpoint}.");
+            Console.WriteLine(">> Waiting for 2 connections to start a game.");
+            Console.ReadKey();
+            // accept clients
+
+
+            // send the sudoku
+
+            // wait for clients to send it back
+
+            // check if it's complete and right
+
+            // send back answer
+
+            // notify if game is over
+
+            // close sockets
+        }
+    }
+}
